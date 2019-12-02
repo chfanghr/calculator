935d1352358581adf66ce2afd8df5224f3133bc2
fanghr <chfanghr@gmail.com>
2019-12-02 22:09:18 +0800

    update example

--- main.cpp
+++ main.cpp
@@ -7,6 +7,8 @@ auto main() -> int {
 	std::cout << Evaluate("1*3+2") << std::endl;
 	std::cout << Evaluate("0+9/3") << std::endl;
 	std::cout << Evaluate("(1+3)/2") << std::endl;
+	std::cout << Evaluate("abs(1.12334)") << std::endl;
+	std::cout << Evaluate("1.45*44%3") << std::endl;
 	std::cout << Evaluate("(1+4)/cos(3.1415926)") << std::endl;
 	return EXIT_SUCCESS;
 }
