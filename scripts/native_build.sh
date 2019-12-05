1d4c14978520f88e0bb43fdc737b9216fc86a66b
fanghr <chfanghr@gmail.com>
2019-12-06 03:13:30 +0800

    add debug and release build to native build script

--- scripts/native_build.sh
+++ scripts/native_build.sh
@@ -2,5 +2,8 @@
 
 set -e
 
-cmake -H. -B"$(uname)_build"
-cmake --build "$(uname)_build"
+cmake -DCMAKE_BUILD_TYPE=Debug -H. -B"$(uname)_build_debug"
+cmake --build "$(uname)_build_debug"
+
+cmake -DCMAKE_BUILD_TYPE=Release -H. -B"$(uname)_build_release"
+cmake --build "$(uname)_build_release"
