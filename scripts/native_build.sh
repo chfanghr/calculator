91c3877d583d370012b297a8517999ac51a6e013
fanghr <chfanghr@gmail.com>
2019-12-06 02:51:39 +0800

    fix native build script

--- scripts/native_build.sh
+++ scripts/native_build.sh
@@ -2,9 +2,5 @@
 
 set -e
 
-NPROCS=$(nproc 2>/dev/null ||
-  sysctl -n hw.ncpu 2>/dev/null ||
-  getconf _NPROCESSORS_ONLN 2>/dev/null)
-
 cmake -H. -B"$(uname)_build"
-cmake --build "$(uname)_build" -j"${NPROCS}"
+cmake --build "$(uname)_build"
