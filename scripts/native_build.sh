48b742875a55f96d1796b1af95fbc85c6d028423
fanghr <chfanghr@gmail.com>
2019-12-06 02:47:39 +0800

    fix script and update ci config

--- scripts/native_build.sh
+++ scripts/native_build.sh
@@ -2,5 +2,9 @@
 
 set -e
 
+NPROCS=$(nproc 2>/dev/null ||
+  sysctl -n hw.ncpu 2>/dev/null ||
+  getconf _NPROCESSORS_ONLN 2>/dev/null)
+
 cmake -H. -B"$(uname)_build"
-cmake --build "$(uname)_build"
+cmake --build "$(uname)_build" -j"${NPROCS}"
