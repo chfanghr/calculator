0c4cf57bd5f26bc9ee95afb08cfc14e8f62928c4
fanghr <chfanghr@gmail.com>
2019-12-06 17:26:44 +0800

    fix windows build script

--- scripts/win32_build.sh
+++ scripts/win32_build.sh
@@ -6,42 +6,6 @@ if [ -d ".__deps" ]; then rm -rf ".__deps"; fi
 
 NPROCS=$(nproc 2>/dev/null || sysctl -n hw.ncpu 2>/dev/null || getconf _NPROCESSORS_ONLN 2>/dev/null)
 
-mkdir .__deps
-cd .__deps
-
-#export CFLAGS=-I"$PWD"/include
-export CPPFLAGS=-I"$PWD"/include
-export LDFLAGS=-L"$PWD"/lib
-export CMAKE_PREFIX_PATH="$PWD"
-
-wget https://ftp.gnu.org/gnu/readline/readline-8.0.tar.gz
-wget https://ftp.gnu.org/pub/gnu/ncurses/ncurses-6.1.tar.gz
-wget http://ftp.gnu.org/gnu/termcap/termcap-1.3.1.tar.gz
-
-tar -xf readline-8.0.tar.gz
-tar -xf ncurses-6.1.tar.gz
-tar -xf termcap-1.3.1.tar.gz
-
-cd termcap-1.3.1
-./configure --prefix="$PWD"/.. --host=x86_64-w64-mingw32
-make -j"${NPROCS}"
-make install
-cd ..
-
-cd ncurses-6.1
-./configure --prefix="$PWD"/.. --host=x86_64-w64-mingw32 --disable-shared --enable-static --enable-term-driver --enable-sp-funcs
-make -j"${NPROCS}"
-make install
-cd ..
-
-cd readline-8.0
-./configure --prefix="$PWD"/.. --host=x86_64-w64-mingw32 --enable-shared --enable-static
-make -j"${NPROCS}"
-make install
-cd ..
-
-cd ..
-
 if [ -d "Win32_build_release" ]; then rm -rf "Win32_build_release"; fi
 if [ -d "Win32_build_debug" ]; then rm -rf "Win32_build_debug"; fi
 if [ -d "Win32_deps" ]; then rm -rf "Win32_deps"; fi
@@ -73,8 +37,8 @@ cd ..
 mkdir Win32_deps
 cd Win32_deps
 
-cp ../.__deps/bin/libreadline8.dll .
-cp /usr/lib/gcc/x86_64-w64-mingw32/*-win32/*.dll . || cp /usr/lib/gcc/x86_64-w64-mingw32/9.2-win32/*.dll .
+cp /usr/lib/gcc/x86_64-w64-mingw32/*-win32/*.dll . || cp \
+  /usr/local/Cellar/mingw-w64/*/toolchain-x86_64/x86_64-w64-mingw32/lib/*.dll .
 
 cd ..
 
