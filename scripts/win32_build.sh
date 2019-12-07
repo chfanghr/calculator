a6f8dfe96d08bb84982f6b4f6c0e5a566264afd2
fanghr <chfanghr@gmail.com>
2019-12-07 22:42:17 +0800

    archive win32 build in script

--- scripts/win32_build.sh
+++ scripts/win32_build.sh
@@ -9,6 +9,7 @@ NPROCS=$(nproc 2>/dev/null || sysctl -n hw.ncpu 2>/dev/null || getconf _NPROCESS
 if [ -d "Win32_build_release" ]; then rm -rf "Win32_build_release"; fi
 if [ -d "Win32_build_debug" ]; then rm -rf "Win32_build_debug"; fi
 if [ -d "Win32_deps" ]; then rm -rf "Win32_deps"; fi
+if [ -d "win32_release" ]; then rm -rf "win32_release"; fi
 
 mkdir Win32_build_release
 cd Win32_build_release
@@ -43,5 +44,21 @@ cp /usr/lib/gcc/x86_64-w64-mingw32/*-win32/*.dll . >/dev/null 2>&1 || cp \
 cd ..
 
 zip -r Win32_build.zip Win32_deps Win32_build_debug Win32_build_release
+
+mkdir win32_release
+cd win32_release
+
+cp ../Win32_deps/libstdc++-*.dll .
+cp ../Win32_deps/libgcc_s_seh-1.dll .
+
+mkdir cmd
+cp ../Win32_build_release/cmd/calc/calc.exe ./cmd/
+
+mkdir examples
+cp ../Win32_build_release/examples/*.exe ./examples
+
+cd ..
+
+zip -r win32_release.zip win32_release
 
 exit 0
