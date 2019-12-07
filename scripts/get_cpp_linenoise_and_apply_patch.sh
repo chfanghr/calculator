a50fd43564a70606c9d2de1f4e2f48b849e727f8
fanghr <chfanghr@gmail.com>
2019-12-07 22:02:21 +0800

    lock dep version

--- scripts/get_cpp_linenoise_and_apply_patch.sh
+++ scripts/get_cpp_linenoise_and_apply_patch.sh
@@ -6,5 +6,6 @@ echo "Applying patch..."
 cd cmd/calc/third_party/cpp-linenoise
 git submodule init
 git submodule update --remote
+git checkout 13e90d3b85e82c8b2ef057d5365340e44397d802 # lock dep version
 git reset --hard
 git apply ../cpp_linenoise_patch_mingw.patch
